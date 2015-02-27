d8bb13d408f6e9ffe20d2ae55d9f5a1ba7e1595f
axton21 <axton_n@yahoo.com>
2015-02-27 14:13:09 -0500

    done

--- reverse_array.rb
+++ reverse_array.rb
@@ -1,1 +1,12 @@
-# your code here
+require 'pry'
+
+class Array
+  def my_reverse
+  index = 1
+    while index < self.size
+    self.unshift(delete_at(index))
+    index+=1
+    end
+    self
+  end
+end
